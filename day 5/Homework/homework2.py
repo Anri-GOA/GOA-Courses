2eecffeb5b3387d159caeab1cad4bf869bf5d8ed
Anri-GOA <160361511+Anri-GOA@users.noreply.github.com>
2024-03-08 02:12:16 +0400

    first commit

---
+++ day 5/Homework/homework2.py
@@ -0,0 +1,13 @@
+a = int(input("Write First Number: "))
+b = int(input("Write Second Number: "))
+
+print(type(a))
+print(type(b))
+
+
+result1 = (a / b)
+result2 = (a // b)
+result3 = (a % b)
+
+print(result1 ,result2, result3)
+  
