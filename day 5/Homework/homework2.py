c4cd4dd513765e9d6aeaee2aa77ec92cedc5d77a
Anri Tsetskhladze <160361511+Anri-GOA@users.noreply.github.com>
2024-03-10 21:45:22 +0400

    homework2.py

--- day 5/Homework/homework2.py
+++ day 5/Homework/homework2.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 a = int(input("Write First Number: "))
 b = int(input("Write Second Number: "))
 
@@ -24,5 +24,3 @@ result2 = (a // b)
 result3 = (a % b)
 
 print(result1 ,result2, result3)
->>>>>>> 2eecffeb5b3387d159caeab1cad4bf869bf5d8ed
-  
