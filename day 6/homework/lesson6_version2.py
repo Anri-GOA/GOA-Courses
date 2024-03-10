6f599a608b2de72e0494008725822d1ceec315c7
Anri Tsetskhladze <160361511+Anri-GOA@users.noreply.github.com>
2024-03-10 21:19:08 +0400

    lesson6_version2.py

---
+++ day 6/homework/lesson6_version2.py
@@ -0,0 +1,55 @@
+x = 10
+y = 5
+
+print(x > y)  
+print(y > x)  
+print(5.5 > 5)  
+print(5 > 5.5)  
+print("a" > "A")  
+
+x = 10
+y = 5
+
+print(x >= y)  
+print(y >= x)  
+print(5.5 >= 5)  
+print(5 >= 5.5)  
+print("A" >= "A")  
+
+x = 10
+y = 5
+
+print(x < y)  
+print(y < x)  
+print(5.5 < 5)  
+print(5 < 5.5)  
+print("b" < "A")  
+
+x = 10
+y = 5
+
+print(x <= y)  
+print(y <= x)  
+print(5.5 <= 5)  
+print(5 <= 5.5)  
+print("B" <= "A")  
+
+x = 10
+y = 5
+z = 10
+
+print(x == y)  
+print(y == x)  
+print(x == z)  
+print(5 == 5.0)  
+print("hello" == "hello")  
+
+x = 10
+y = 5
+z = 10
+
+print(x != y)  
+print(y != x)  
+print(x != z)  
+print(5 != 5.0)  
+print("hello" != "hello Universe")  
